9439af4e932488fa5e860ed86160ed0826450bb7
yunior <yunior0509av@gmail.com>
2018-12-23 18:51:21 -0400

    Add standart view

---
+++ locallibrary/locallibrary/SubjectOrganizer/data.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup as bs
+import urllib.request as uReq
+import requests
+
+
+def get_data(subject_id):
+    url_cookies = 'http://www.utesa.edu/webutesa/recintos/Santiago/HomeSG.asp?reci=UTSAN'
+    r = requests.get(url_cookies,timeout=5)
+    for cookie in r.cookies:
+        cook = str(cookie)[8:-20]
+
+    url = "http://www.utesa.edu/webutesa/recintos/InfGen/Horarios2.asp?FDesde=%s&Fhasta=%s&x=65&y=5&SelCiclo=12019"
+    opener = uReq.build_opener()  
+    opener.addheaders.append(('Cookie', cook))
+    url_e = (url % (subject_id, subject_id + "999"))
+    page = opener.open(url_e)
+    page_html = page.read()
+    page.close()
+
+    page_soup = bs(page_html, "html.parser")
+    table = page_soup.find("td", {"height": "238"})
+    rows = table.findAll("tr")
+    subject = {'groups': []}
+
+    for row in rows:
+        data = row.findAll("td", {"valign": "middle"})
+        count = 0
+        group = {}
+        for info in data:
+            if count == 3:
+                subject['name'] = info.text.strip()
+            if count == 1:
+                group['id'] = info.text.strip()
+            if count == 5:
+                group['time'] = info.text.strip()
+            if count == 6:
+                group['classroom'] = info.text.strip()
+            count +=1
+        if group:
+            subject['groups'].append(group)
+
+    class materia:
+        def __init__(self, name, groups):
+            self.name = name
+            self.groups = groups
+
+    class grupo:
+        def __init__(self, id_g, time, classroom):
+            self.id = id_g
+            self.time = time
+            self.classroom = classroom
+
+    subject_ob = materia(subject['name'], [])
+
+    for g in subject['groups']:
+        gup = grupo(g['id'], g['time'], g['classroom'])
+        subject_ob.groups.append(gup)
+    
+    return subject_ob
+
+if __name__ == "__main__":
+    ##example with id of ALGORITMOS COMPUTACIONALES INF117
+    print(get_data('INF117'))
--- locallibrary/locallibrary/SubjectOrganizer/views.py
+++ locallibrary/locallibrary/SubjectOrganizer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from . import data
 
 # Create your views here.
+def index(request):
+    return subjects(request)
+
+def subjects(request):
+    template = loader.get_template("SO/index.html")
+    subject_data = data.get_data("CON116")
+    context = {
+        'data': subject_data
+    }
+
+    return HttpResponse(template.render(context, request))

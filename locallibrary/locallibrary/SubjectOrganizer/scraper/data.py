41a5454e530117e1d81958d75fb9fb1d6f0330b9
yunior <yun05@github.com>
2019-01-07 09:42:53 -0400

    Add ciclo like parameter, select table by css selector

--- locallibrary/locallibrary/SubjectOrganizer/scraper/data.py
+++ locallibrary/locallibrary/SubjectOrganizer/scraper/data.py
@@ -4,21 +4,8 @@ import requests
 import objects
 
 # Constants
-URL = "http://www.utesa.edu/webutesa/recintos/InfGen/Horarios2.asp?FDesde=%s&Fhasta=%s&x=65&y=5&SelCiclo=12019"
+URL = "http://www.utesa.edu/webutesa/recintos/InfGen/Horarios2.asp?FDesde=%s&Fhasta=%s&x=65&y=5&SelCiclo=%s"
 URL_COOKIES = 'http://www.utesa.edu/webutesa/recintos/Santiago/HomeSG.asp?reci=UTSAN'
-
-# class Subject:
-#     def __init__(self, name, groups):
-#         self.name = name
-#         self.groups = groups
-
-
-# class Group:
-#     def __init__(self, id_group='', time='', classroom='', is_virtual=False):
-#         self.id = id_group
-#         self.time = time
-#         self.classroom = classroom
-#         self.is_virtual = is_virtual
 
 def get_cookies(url):
     r = requests.get(url, timeout=5)
@@ -35,17 +22,16 @@ def get_html(url, cookies):
     return page_html
 
 
-def get_data(subject_id, url, subject_data = None):
+def get_data(subject_id, ciclo, url, subject_data = None):
     cookies = get_cookies(URL_COOKIES)
 
     if subject_data == None:
-        url = (url % (subject_id, subject_id + "999"))
+        url = (url % (subject_id, subject_id + "999", ciclo))
         subject_data = objects.Subject('', [])
 
     page_html = get_html(url, cookies)
-
     page_soup = bs(page_html, "html.parser")
-    table = page_soup.find("td", {"height": "238"})
+    table = page_soup.select_one("body > table > tr:nth-of-type(7) > td:nth-of-type(1)")
     rows = table.findAll("tr")
 
     for row in rows:
@@ -53,7 +39,6 @@ def get_data(subject_id, url, subject_data = None):
         group_data = objects.Group()
         
         if data:
-            print('ID: %s and Status: %s' % (data[1].text.strip(), data[0].text.strip()))
             status = data[0].text.strip()
             subject_data.name = data[3].text.strip()
             group_data.id = data[1].text.strip()
@@ -63,18 +48,18 @@ def get_data(subject_id, url, subject_data = None):
                 group_data.is_virtual = True
             
             subject_data.groups.append(group_data)
-    
-    next_page = table.select_one('img[src*="../../images/PagSig.gif"]')
+
+    next_page = table.select_one("img[src*='../../images/PagSig.gif']")
     if next_page:
         new_url = 'http://www.utesa.edu' + next_page.parent.get('href')
-        return get_data(subject_id, new_url, subject_data)
+        return get_data(subject_id, ciclo, new_url, subject_data)
     else:
         return subject_data
 
 
 if __name__ == "__main__":
-    # example to print data of ESP301  
-    data = get_data('ESP301', URL)
+    # example to print data of ESP301, and ciclo: 12019
+    data = get_data('ESP301', '12019', URL)
     print('Subject name: %s' % data.name)
     print("=============================")
     for group in data.groups:
@@ -83,3 +68,4 @@ if __name__ == "__main__":
             (group.id, group.time, group.classroom, group.is_virtual))
         print("====================================================================")
     print("FINISH")
+    

367b86116a9deb029354a1fa2fed9218646f3718
Yunior Almengo <yalmengo@intellisys.com.do>
2019-09-25 15:42:25 -0400

    Improve data.py script

--- locallibrary/locallibrary/SubjectOrganizer/data.py
+++ locallibrary/locallibrary/SubjectOrganizer/data.py
@@ -9,7 +9,7 @@ def get_data(subject_id):
     for cookie in r.cookies:
         cook = str(cookie)[8:-20]
 
-    url = "http://www.utesa.edu/webutesa/recintos/InfGen/Horarios2.asp?FDesde=%s&Fhasta=%s&x=65&y=5&SelCiclo=12019"
+    url = "http://www.utesa.edu/webutesa/recintos/InfGen/Horarios2.asp?FDesde=%s&Fhasta=%s&x=65&y=5&SelCiclo=32019"
     opener = uReq.build_opener()  
     opener.addheaders.append(('Cookie', cook))
     url_e = (url % (subject_id, subject_id + "999"))
@@ -55,9 +55,16 @@ def get_data(subject_id):
     for g in subject['groups']:
         gup = grupo(g['id'], g['time'], g['classroom'])
         subject_ob.groups.append(gup)
-    
     return subject_ob
 
+
+def printData(value):
+    data = get_data(value);
+    print(data.name)
+    for group in data.groups:
+        print(group.id)
+        print(group.time)
+        print(group.classroom)
 if __name__ == "__main__":
     ##example with id of ALGORITMOS COMPUTACIONALES INF117
-    print(get_data('INF117'))
+    print(printData('INF117'))
